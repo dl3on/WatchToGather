7ab07b9a2e922fa83d45ac32b5eef3f5bfd07549
horouuu <horou@coronne.io>
2025-11-12 18:44:32 +0800

    feat: signal manager prototype

---
+++ signalling/lib/signal-manager.ts
@@ -0,0 +1,70 @@
+import { SocketOptions } from "dgram";
+import { io, ManagerOptions, Socket } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  EServerToClientEvents,
+  Response,
+  ServerToClientEvents,
+  ResponseType,
+} from "../types";
+export class SignalManager {
+  _peerId: string;
+  _socket: Socket<ServerToClientEvents, ClientToServerEvents>;
+  _verbose: boolean;
+  constructor(
+    peerId: string,
+    serverUrl: string,
+    socketOptions: Omit<Partial<ManagerOptions & SocketOptions>, "autoConnect">,
+    verbose = false
+  ) {
+    this._peerId = peerId;
+    this._socket = io(serverUrl, { ...socketOptions, autoConnect: false });
+    this._verbose = verbose;
+  }
+
+  public connect() {
+    try {
+      this._socket.connect();
+      this.updateListeners();
+    } catch (e) {
+      console.error("Error connecting to socket:", e);
+    }
+  }
+
+  private onConnect() {
+    if (this._verbose)
+      console.log(
+        `Connected to signalling server.\nSocket ID: ${this._socket.id}`
+      );
+  }
+
+  private onJoinResponse(res: Response<ResponseType.Join>) {
+    if (this._verbose) {
+      if (res.success) {
+        console.log(`Received response from server: ${res.body}`);
+      } else {
+        console.log(`Failed to join room ${res.roomId}:`, res.errMsg);
+      }
+    }
+  }
+
+  private onHostResponse(res: Response<ResponseType.Host>) {
+    if (this._verbose) {
+      if (res.success) {
+        console.log(`Successfully created Room ${res.roomId}`);
+      } else {
+        console.log(`Failed to create room:`, res.errMsg);
+      }
+    }
+  }
+
+  private updateListeners() {
+    if (this._socket) {
+      this._socket.on("connect", this.onConnect);
+      this._socket.on(EServerToClientEvents.JoinResponse, this.onJoinResponse);
+      this._socket.on(EServerToClientEvents.HostResponse, this.onHostResponse);
+    }
+  }
+
+  resetListeners() {}
+}
