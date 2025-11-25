0f7ae86e230ba50d9bd3cda927bf75b580d09094
dl3on <124175480+dl3on@users.noreply.github.com>
2025-11-25 22:04:51 +0700

    feat: MessageManager singleton

--- src/p2p/lib/message-manager.ts
+++ src/p2p/lib/message-manager.ts
@@ -2,11 +2,25 @@ import { randomUUID } from "node:crypto";
 import { PeerMessage, PeerMessageType } from "../../common/peer-messages-types";
 
 export class MessageManager {
+  private static _instance: MessageManager | null;
   _peerId: string;
   _peersChannel: RTCDataChannel[];
   constructor(peerId: string, peersChannel: RTCDataChannel[]) {
     this._peerId = peerId;
     this._peersChannel = peersChannel;
+  }
+
+  public static getInstance(
+    peerId: string,
+    peersChannel: RTCDataChannel[]
+  ): MessageManager {
+    if (!MessageManager._instance) {
+      const newInstance = new MessageManager(peerId, peersChannel);
+      MessageManager._instance = newInstance;
+      return newInstance;
+    } else {
+      return MessageManager._instance;
+    }
   }
 
   sendToAll(eventType: PeerMessageType, time: number) {
