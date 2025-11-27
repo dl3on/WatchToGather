65608e366cfef7345fe88f93dc95b8822f05090a
dl3on <124175480+dl3on@users.noreply.github.com>
2025-11-27 19:40:20 +0700

    feat: video element observer in content script

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,16 +1,21 @@
+import { sendChromeMsg } from "./lib/chrome";
 import { VideoController } from "./lib/video-controller";
 
-const video = document.querySelector("video");
-if (!video) {
-  console.error("No video element found");
-} else {
-  const videoController = new VideoController(video);
+const observer = new MutationObserver(() => {
+  const video = document.querySelector("video");
+  if (video) {
+    console.log("Found VIDEO");
+    observer.disconnect();
+    const videoController = new VideoController(video);
 
-  chrome.runtime.sendMessage({ type: "content-ready" });
+    // TODO: notify peers ready state
+    sendChromeMsg({ type: "content-ready" });
 
-  chrome.runtime.onMessage.addListener((msg) => {
-    if (msg.type === "video-actions") {
-      videoController.onRemoteEvent(msg.payload);
-    }
-  });
-}
+    chrome.runtime.onMessage.addListener((msg) => {
+      if (msg.type === "VIDEO_ACTIONS") {
+        videoController.onRemoteEvent(msg.payload);
+      }
+    });
+  }
+});
+observer.observe(document.body, { childList: true, subtree: true });
