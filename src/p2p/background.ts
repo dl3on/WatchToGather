7b0dcd87485d7d1ba60623203a79b254992c98b3
dl3on <124175480+dl3on@users.noreply.github.com>
2025-12-02 19:52:26 +0700

    fix: wrapped code in async function init()

--- src/p2p/background.ts
+++ src/p2p/background.ts
@@ -15,84 +15,88 @@ async function ensureOffscreen() {
   });
 }
 
-ensureOffscreen();
+async function init() {
+  await ensureOffscreen();
 
-let controlledTabId: number | null = null;
-let pendingTabId: number | null = null;
-let isInRoom = false;
+  let controlledTabId: number | null = null;
+  let pendingTabId: number | null = null;
+  let isInRoom = false;
 
-const data = await loadVCStates();
-controlledTabId = data.controlledTabId;
-isInRoom = data.isInRoom;
+  const data = await loadVCStates();
+  controlledTabId = data.controlledTabId;
+  isInRoom = data.isInRoom;
 
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === "IN_ROOM") {
-    isInRoom = true;
-
-    saveState();
-    return;
-  }
-
-  if (msg.type === "REGISTER_TAB") {
-    registerActiveTab();
-    return;
-  }
-
-  if (msg.type === "VC_STATUS") {
-    if (msg.success) {
-      controlledTabId = pendingTabId;
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.type === "IN_ROOM") {
+      isInRoom = true;
 
       saveState();
       return;
     }
-  }
 
-  // TODO: clear storage instead
-  if (msg.type === "LEFT_ROOM") {
-    controlledTabId = null;
-    isInRoom = false;
-
-    saveState();
-    return;
-  }
-
-  if (msg.type === "VIDEO_ACTIONS") {
-    // Relay messages from offscreen to content script
-    if (controlledTabId !== null) {
-      forwardVideoActionsMsg(controlledTabId, msg);
-    } else {
-      console.log("[ERROR] No tab registered");
-    }
-    return;
-  }
-});
-
-chrome.tabs.onRemoved.addListener((tabId) => {
-  if (tabId === controlledTabId) {
-    controlledTabId = null;
-    saveState();
-  }
-});
-
-function saveState() {
-  saveVCStates(controlledTabId, isInRoom);
-}
-
-/** Automatically registers current active tab
- * if it has a video element
- * with an option to re-register a new tab */
-function registerActiveTab() {
-  if (!isInRoom) return;
-
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tabId = tabs[0]?.id;
-    if (!tabId) {
-      console.log("No active tab to register");
+    if (msg.type === "REGISTER_TAB") {
+      registerActiveTab();
       return;
     }
 
-    console.log("Validating current tab:", tabId);
-    sendPrepareVcMsg(tabId);
-    pendingTabId = tabId;
+    if (msg.type === "VC_STATUS") {
+      if (msg.success) {
+        controlledTabId = pendingTabId;
+
+        saveState();
+        return;
+      }
+    }
+
+    // TODO: clear storage instead
+    if (msg.type === "LEFT_ROOM") {
+      controlledTabId = null;
+      isInRoom = false;
+
+      saveState();
+      return;
+    }
+
+    if (msg.type === "VIDEO_ACTIONS") {
+      // Relay messages from offscreen to content script
+      if (controlledTabId !== null) {
+        forwardVideoActionsMsg(controlledTabId, msg);
+      } else {
+        console.log("[ERROR] No tab registered");
+      }
+      return;
+    }
   });
+
+  chrome.tabs.onRemoved.addListener((tabId) => {
+    if (tabId === controlledTabId) {
+      controlledTabId = null;
+      saveState();
+    }
+  });
+
+  function saveState() {
+    saveVCStates(controlledTabId, isInRoom);
+  }
+
+  /** Automatically registers current active tab
+   * if it has a video element
+   * with an option to re-register a new tab */
+  function registerActiveTab() {
+    if (!isInRoom) return;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tabId = tabs[0]?.id;
+      if (!tabId) {
+        console.log("No active tab to register");
+        return;
+      }
+
+      console.log("Validating current tab:", tabId);
+      sendPrepareVcMsg(tabId);
+      pendingTabId = tabId;
+    });
+  }
 }
+
+init();
