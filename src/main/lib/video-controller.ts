87b5beca1585ab0e98e0e473a66f2d6670b58b39
dl3on <124175480+dl3on@users.noreply.github.com>
2025-12-04 00:26:43 +0700

    feat: SEEK with message echo suppression

--- src/main/lib/video-controller.ts
+++ src/main/lib/video-controller.ts
@@ -2,14 +2,14 @@ import { PeerMessage, PeerMessageType } from "../../common/sync-messages-types";
 import { sendVCMsg } from "./chrome";
 
 export class VideoController {
+  private _ignoreSeekCount = 0;
   _video: HTMLVideoElement;
   constructor(videoElement: HTMLVideoElement) {
     this._video = videoElement;
 
     this._video.addEventListener("pause", () => this.onPause());
     this._video.addEventListener("play", () => this.onPlay());
-    this._video.addEventListener("seek", () => this.onSeek());
-    // TODO: listen to incoming msg from message manager
+    this._video.addEventListener("seeked", () => this.onSeek());
   }
 
   onPause() {
@@ -27,6 +27,8 @@ export class VideoController {
   }
 
   onSeek() {
+    if (this._ignoreSeekCount > 0) return;
+
     sendVCMsg({
       type: PeerMessageType.Seek,
       time: this._video.currentTime,
@@ -34,18 +36,34 @@ export class VideoController {
   }
 
   onRemoteEvent(msg: PeerMessage) {
-    console.log(`[VC] ${msg}`);
     // TODO: pause/play/seek video; ignore if video is already in the same state as described in msg
+    // Current observations: pause&play action is being echoed by other peer so sender also receives the same msg w/ diff mid.
+    // its fine for now because video.pause and video.play has no effect hence no further messages are echoed.
     switch (msg.type) {
       case PeerMessageType.Pause:
+        console.log("[VC] PAUSE");
         this._video.pause();
         break;
 
       case PeerMessageType.Play:
+        console.log("[VC] PLAY");
         this._video.play();
         break;
 
       case PeerMessageType.Seek:
+        if (Math.abs(this._video.currentTime - msg.time) < 0.3) return;
+
+        console.log("[VC] SEEK");
+        this._ignoreSeekCount++;
+        this._video.currentTime = msg.time;
+
+        this._video.addEventListener(
+          "seeked",
+          () => {
+            this._ignoreSeekCount--;
+          },
+          { once: true }
+        );
         break;
     }
   }
